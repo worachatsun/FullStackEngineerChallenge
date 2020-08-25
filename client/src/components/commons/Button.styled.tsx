c86696be59fde654be4849cd3151ac29b4b5c4e6
worachatsun <worachat.sun@gmail.com>
2020-08-26 01:12:18 +0900

    Chagne button and input style

--- client/src/components/commons/Button.styled.tsx
+++ client/src/components/commons/Button.styled.tsx
@@ -4,7 +4,6 @@ export const ButtonContainer = styled.button`
   background: #ff0033;
   border: 0;
   color: white;
-  margin: 20px;
   padding: 10px;
   font-size: 1em;
   border-radius: 5px;
--- client/src/components/commons/Input.styled.tsx
+++ client/src/components/commons/Input.styled.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const InputContainer = styled.input`
   border: 0;
   border-bottom: solid 2px #f03;
-  margin: 15px 0 15px 0;
+  margin: 15px;
+  width: 80%;
   outline: none;
   font-size: 0.95em;
 `;
