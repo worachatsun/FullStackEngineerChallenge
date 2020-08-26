8247d27a378b18df5f2427ab441660cc390d8103
worachatsun <worachat.sun@gmail.com>
2020-08-27 04:44:23 +0900

    Update header style

--- client/src/components/commons/Header.styled.tsx
+++ client/src/components/commons/Header.styled.tsx
@@ -5,4 +5,20 @@ export const HeaderContainer = styled.div`
   width: 100%;
   height: 73.81px;
   border-bottom: solid 8px #f03;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LogoContainer = styled.div`
+  margin-left: 10px;
+`;
+
+export const LogoutContainer = styled.div`
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+export const UserContainer = styled.div`
+  margin-right: 10px;
 `;
--- client/src/components/commons/Header.tsx
+++ client/src/components/commons/Header.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import { HeaderContainer } from './Header.styled';
+import React, { useContext, FunctionComponent } from 'react';
+import {
+  HeaderContainer,
+  LogoContainer,
+  LogoutContainer,
+  UserContainer,
+} from './Header.styled';
+import { UserContext } from '../../context/UserContext';
+import Button from './Button';
+import { useHistory } from 'react-router-dom';
 
-const Header = () => {
-  return <HeaderContainer>sun</HeaderContainer>;
+const Header: FunctionComponent = () => {
+  const { state } = useContext(UserContext);
+  const history = useHistory();
+
+  const onClickLogout = () => {
+    localStorage.removeItem('token');
+    history.push('/');
+  };
+
+  const onClickLogo = () => {
+    history.push('/list');
+  };
+
+  return (
+    <HeaderContainer>
+      <LogoContainer onClick={onClickLogo}>
+        <img src='img_logo.png' />
+      </LogoContainer>
+      <LogoutContainer>
+        <UserContainer>{state.username}</UserContainer>
+        <Button onClick={onClickLogout}>Logout</Button>
+      </LogoutContainer>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
