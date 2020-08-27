61b5d397f7fa57d9d7de5ddf4ae83e7d134fc453
worachatsun <worachat.sun@gmail.com>
2020-08-28 05:48:10 +0900

    Add performance page

--- client/src/components/Pages/Performance/Performance.styled.tsx
+++ client/src/components/Pages/Performance/Performance.styled.tsx
@@ -13,16 +13,17 @@ export const Container = styled.div`
 
 export const Lable = styled.div`
   font-size: 1.5em;
+  margin-bottom: 20px;
 `;
 
-export const Table = styled.table`
+export const Table = styled.div`
   text-align: left;
   width: 90%;
   padding: 20px;
   box-shadow: 2px 0px 4px 0px rgba(0, 0, 0, 0.1);
 `;
 
-export const Wrapper = styled.table`
+export const Wrapper = styled.div`
   display: 'flex';
 `;
 
@@ -36,6 +37,15 @@ export const Tr = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-top: 10px;
+  height: 40px;
+  border: 1px solid #ff0033;
+  border-radius: 5px;
+  color: #ff0033;
+
+  &:hover {
+    color: white;
+    background: #ff0033;
+  }
 `;
 
 export const Hr = styled.hr`
@@ -45,4 +55,18 @@ export const Hr = styled.hr`
 
 export const ThText = styled.div`
   margin-left: 10px;
+`;
+
+export const TrText = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  width: 50%;
+`;
+
+export const AnswerContainer = styled.div`
+  display: flex;
+  width: 50%;
+  justify-content: space-around;
+  font-size: 1em;
 `;
--- client/src/components/Pages/Performance/index.tsx
+++ client/src/components/Pages/Performance/index.tsx
@@ -1,31 +1,66 @@
 import React, { FunctionComponent } from 'react';
 import {
-  Container,
   Lable,
   Table,
-  Wrapper,
   Hr,
   Th,
   ThText,
   Tr,
+  TrText,
+  AnswerContainer,
 } from './Performance.styled';
 import Layout from '../../Layout';
+import useSWR from 'swr';
+import { REVIEWED_LIST_API } from '../../../constants/routes';
+import { fetcher } from '../../commons/utils/client';
+import { IReviewModel } from '../ListReviews';
+import { FaAngleRight } from 'react-icons/fa';
+
+interface IPerformanceModel extends IReviewModel {
+  answers: IAnswerPayload[];
+}
+
+interface IAnswerPayload {
+  answer: string;
+  createdAt: Date;
+  id: number;
+  questionId: number;
+  reviewId: number;
+  updatedAt: Date;
+}
 
 const Performance: FunctionComponent = () => {
-  return (
+  const token = localStorage.getItem('token');
+  const { data = [] } = useSWR<IPerformanceModel[]>(
+    [REVIEWED_LIST_API, token],
+    fetcher
+  );
+  console.log(data);
+  return data ? (
     <Layout>
       <Lable>Performance reviews</Lable>
       <Table>
-        <Th>
-          <ThText>Name</ThText>
-        </Th>
-        <Hr />
-        <Tr>
-          <div>username</div>
-        </Tr>
-        <Hr />
+        {data.map((review: IPerformanceModel, index: number) => (
+          <Tr key={index}>
+            <TrText>
+              {review.reviewBy}
+              <FaAngleRight />
+              {review.reviewTo}
+            </TrText>
+            <AnswerContainer>
+              {review.answers.map((answer: IAnswerPayload, index: number) => (
+                <div key={index}>
+                  {`Question${answer.questionId}: `}
+                  {answer.answer}
+                </div>
+              ))}
+            </AnswerContainer>
+          </Tr>
+        ))}
       </Table>
     </Layout>
+  ) : (
+    <div></div>
   );
 };
 
